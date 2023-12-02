ad5fea927be5a6610e4c3545b4fc341e3f16e927
pandupan <pangestup60@gmail.com>
2023-12-02 09:32:38 +0700

    [FIX] Text Color rename

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import AboutUs from '@/components/pages/LandingPage/AboutUs'
 import ContactUs from '@/components/pages/LandingPage/ContactUs'
 import HeroSectionLP from '@/components/pages/LandingPage/HeroSectionLP'
 import ReceiveNews from '@/components/pages/LandingPage/ReceiveNews'
-import SelectTheme from '@/components/pages/LandingPage/SelectTheme'
+import SelectTheme from '@/components/pages/LandingPage/CekBrand'
 
 export default function Home() {
   return (
